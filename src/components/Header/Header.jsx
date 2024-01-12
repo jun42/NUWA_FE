a686ec5b1ba047e3c51f7b330dc1033f486466a1
Jun <yuncheol.kim42@gmail.com>
2024-01-12 21:46:04 +0900

    test: pr template 확인용

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import useBoundStore from '../../store/store';
 
 const Header = () => {
   const bearPopulation = useBoundStore((state) => state.bears);
-  return <div>Header d{bearPopulation}</div>;
+  return <div>Header {bearPopulation}</div>;
 };
-
+// pr template 확인용
 export default Header;
