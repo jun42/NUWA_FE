5cd0f05e2ff640edade6b68b454f86ed88aca0d6
Jun <yuncheol.kim42@gmail.com>
2024-01-18 00:11:13 +0900

    Revert "design: 예시 팔레트"
    This reverts commit c81ef304ec6029b0fd2e85fb3bb8263fa9df563e.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,52 +1,8 @@
-import styled from 'styled-components';
 import useBoundStore from '../../store/store';
 
 const Header = () => {
   const bearPopulation = useBoundStore((state) => state.bears);
-  const primary = [
-    `--primary-100 `,
-    `--primary-200 `,
-    `--primary-300 `,
-    `--primary-400`,
-    `--primary-500 `,
-    `--primary-600 `,
-    `--primary-700 `,
-  ];
-
-  const grey = [
-    `--grey-100 `,
-    `--grey-200 `,
-    `--grey-300 `,
-    `--grey-400`,
-    `--grey-500 `,
-    `--grey-600 `,
-    `--grey-700 `,
-  ];
-  const variant = [100, 200, 300, 400, 500, 600, 700];
-
-  return (
-    <>
-      <div>bear population:{bearPopulation}</div>
-      {primary.map((item, index) => (
-        <Palette key={index} backgroundColor={item} />
-      ))}
-      {variant.map((item, index) => (
-        <Palette2 key={index} value={item} />
-      ))}
-    </>
-  );
+  return <div>Header {bearPopulation}</div>;
 };
 // pr template 확인용
 export default Header;
-
-const Palette = styled.div`
-  width: 1rem;
-  height: 1rem;
-  background-color: var(${(props) => props.backgroundColor});
-`;
-
-const Palette2 = styled.div`
-  width: 1rem;
-  height: 1rem;
-  background-color: ${({ theme, value }) => theme.primary[value]};
-`;
