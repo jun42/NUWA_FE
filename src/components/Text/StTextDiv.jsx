6732e222b4f0a2bfc8bfbab88d7d12343094882a
Jun <yuncheol.kim42@gmail.com>
2024-01-26 19:09:42 +0900

    design: text 스타일 컴포넌트

---
+++ src/components/Text/StTextDiv.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+/**
+ * @param {string} color ex)primary400
+ * @param {number} weight ex) 100 200 300 400 500 600 700
+ * @param {number} size (단위: px) ex) 16 32 /
+ */
+const StTextDiv = styled.div`
+  color: ${({ theme, $color = 'grey700' }) => theme.color[$color]};
+  font-weight: ${(props) => props.$weight};
+  font-size: ${(props) => props.$size}px; // px
+`;
+
+export default StTextDiv;
