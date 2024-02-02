e681da75a4f4c6913396bd145fa8671e115e271c
김형석 <kimhyoungseok@gimhyeongseog-MacBook-Pro.local>
2024-02-03 04:43:59 +0900

    design 메인페이지 작업중

---
+++ src/pages/Feature/FeatCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FeatCard = () => {
+  return (
+    <StCardContainer>
+
+    </StCardContainer>
+  )
+}
+
+export default FeatCard
+
+const StCardContainer = styled.div`
+  width: 50%;
+  height: 550px;
+  border: 1px solid red;
+  border-radius: 20px;
+`;
