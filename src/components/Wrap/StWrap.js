b7213728da82d5daec11599c4e55876602b5a72c
seon-mikim <tjsalsun@gmail.com>
2024-02-05 21:54:17 +0900

    feat: Wrap 컴포넌트 css 추가

---
+++ src/components/Wrap/StWrap.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import Wrap from './Wrap';
+
+export const StWrap = styled(Wrap)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  div:first-child {
+    margin-top: 56px;
+  }
+`;
+
+export const ButtonWrap = styled(Wrap)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 30px;
+  button {
+    margin-bottom: 12px;
+  }
+  a {
+    color: #575dfb;
+    text-decoration: underline;
+    font-weight: 600;
+  }
+`;
