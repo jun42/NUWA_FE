c189cd6d5ddd2dc0bf4f7358e5af4e7e61279648
seon-mikim <tjsalsun@gmail.com>
2024-02-08 11:56:05 +0900

    feat: 스텝 컴포넌트 추가

--- src/pages/createWorkspace/index.jsx
+++ src/pages/createWorkspace/index.jsx
@@ -1,9 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { StSection } from '@components/Section/StSection';
+import Step from '@components/Step/Step';
+import { WORKERSPACE_STEP } from '../../constants/workStep/WORKSPACE_STEP';
 
 const CreateWorkspace = () => {
+  const pathName = useLocation().pathname;
+ 
   return (
     <StSection>
+      <Step isStep={pathName} step={WORKERSPACE_STEP}/>
       <Outlet />
     </StSection>
   );
