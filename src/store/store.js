af80ecccf752bbee6547c1dc0c5470394378dddb
Jun <yuncheol.kim42@gmail.com>
2024-02-14 21:12:46 +0900

    fix:오타수정

--- src/store/store.js
+++ src/store/store.js
@@ -4,14 +4,14 @@ import { createJSONStorage, devtools, persist } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 import { createSocialSignupSlice } from './socialSignupSlice';
 
-const persisitKeys = ['email', 'provider'];
+const persistKeys = ['email', 'provider'];
 
 const persistOption = {
   name: 'NUWA-Storage', // name of the item in the storage (must be unique)
   storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
   partialize: (state) =>
     Object.fromEntries(
-      Object.entries(state).filter(([key]) => persisitKeys.includes(key))
+      Object.entries(state).filter(([key]) => persistKeys.includes(key))
     ),
 };
 
