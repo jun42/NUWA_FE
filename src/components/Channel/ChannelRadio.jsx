307e21cd728a058fd114e402631e79582e5e585f
Jun <yuncheol.kim42@gmail.com>
2024-02-14 19:34:02 +0900

    style: RadioConstants 주석 추가

--- src/components/Channel/ChannelRadio.jsx
+++ src/components/Channel/ChannelRadio.jsx
@@ -1,5 +1,11 @@
 import { Flex, Image, Radio, RadioGroup, Stack } from '@chakra-ui/react';
-
+/**
+ *
+ * @param {string} name
+ * @param {string} value : react state
+ * @param {SetStateAction} state react setState
+ * @param {Array} RadioConstants 라디오 선택지 데이터 담긴 상수 배열
+ */
 const ChannelRadio = ({ name, RadioConstants, value, setValue }) => {
   return (
     <Flex flexDirection={'column'} gap={'14px'} pb={'4rem'} fontSize={'14px'}>
