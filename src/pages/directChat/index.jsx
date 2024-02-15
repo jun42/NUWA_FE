023b1007299e387c13831a3a03080621054cd53e
Jun <yuncheol.kim42@gmail.com>
2024-02-15 18:22:37 +0900

    design:임시커밋

--- src/pages/directChat/index.jsx
+++ src/pages/directChat/index.jsx
@@ -1,12 +1,14 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Textarea } from '@chakra-ui/react';
 import DirectChatHeader from './DirectChatHeader';
 
 const DirectChatPage = () => {
   return (
     <Box width="100%" p={'0.5rem'}>
       <DirectChatHeader />
-      <Box></Box>
-      <Box></Box>
+      <Box height={'70vh'} border={'1px'}></Box>
+      <Box border={'1px'} height={'15vh'}>
+        <Textarea resize={'none'} />
+      </Box>
     </Box>
   );
 };
