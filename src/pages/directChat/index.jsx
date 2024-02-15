203f9c5c60b185a8647d3192cddeb7e63bcad10d
Jun <yuncheol.kim42@gmail.com>
2024-02-15 13:20:37 +0900

    design: directChat 헤더 스케치

---
+++ src/pages/directChat/index.jsx
@@ -0,0 +1,14 @@
+import { Box } from '@chakra-ui/react';
+import DirectChatHeader from './DirectChatHeader';
+
+const DirectChatPage = () => {
+  return (
+    <Box width="100%" p={'0.5rem'}>
+      <DirectChatHeader />
+      <Box></Box>
+      <Box></Box>
+    </Box>
+  );
+};
+
+export default DirectChatPage;
