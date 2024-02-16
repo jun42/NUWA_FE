8f866427f194dc322c951299851b7b24a7540eec
suj2n <suj2n.k@gmail.com>
2024-02-16 21:17:33 +0900

    design/ 대쉬보드

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from 'react'
+import { Grid } from '@chakra-ui/react'
+import TopSection from './TopSection' 
+import styled from 'styled-components'
 
-const DashBoard = () => {
-  return <div>dashboard</div>;
-};
 
-export default DashBoard;
+const dashBoard = () => {
+  return (
+    <StContainer>
+      <Grid templateRows="" gap={6} >
+      <TopSection/>
+        {/* <BottomSection/> */}
+
+      </Grid>
+       
+    </StContainer>
+  )
+}
+
+export default dashBoard;
+
+
+const StContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    display: grid;
+`;
