4136a49c22ccadb2f672c86ff5035d8bbfd20926
seon-mikim <tjsalsun@gmail.com>
2024-02-20 18:17:20 +0900

    feat: 사용자 정보 미입력시 알럿 모달 구현

--- src/components/Modal/ProfileModal/create/CreateProfileImage.jsx
+++ src/components/Modal/ProfileModal/create/CreateProfileImage.jsx
@@ -1,12 +1,22 @@
 import Modal from '../../Modal';
+import ConfirmFooter from './ConfirmFooter';
 import ModalBody from './ModalBody';
 import ModalFooter from './ModalFooter';
 
-const CreateProfileImage = ({ isOpen, onClose }) => {
+const CreateProfileImage = ({ isOpen, onClose, userInfo }) => {
+  const { userName, userJob } = userInfo;
+  const isEmptyProfile = !userName && !userJob;
+  if (isEmptyProfile)
+    return (
+      <Modal isOpen={isOpen} onClose={onClose} modalTitle="사용자 정보 입력 확인">
+        사용자 이름 또는 직무를 입력하세요
+        <ConfirmFooter onClose={onClose}/>
+      </Modal>
+    );
   return (
     <Modal isOpen={isOpen} onClose={onClose} modalTitle="프로필 설정">
-      <ModalBody />
-      <ModalFooter onClose={onClose}/>
+      <ModalBody userInfo={userInfo} />
+      <ModalFooter onClose={onClose} />
     </Modal>
   );
 };
