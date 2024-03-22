f8bdbfee3449ec7ef7312e9637caee922061a7cc
Jun <yuncheol.kim42@gmail.com>
2024-03-22 17:32:42 +0900

    feat: s3 수정

--- src/apis/axios/axiosImage.js
+++ src/apis/axios/axiosImage.js
@@ -18,3 +18,33 @@ const createInstance = () => {
 };
 
 export const axiosImgInstance = createInstance();
+
+import AWS from 'aws-sdk';
+
+export const uploadS3 = (imageFile) => {
+  const formData = new FormData();
+  formData.append('file', imageFile);
+  formData.append('name', imageFile.name);
+
+  const REGION = import.meta.env.VITE_APP_REGION;
+  const ACESS_KEY_ID = import.meta.env.VITE_APP_ACCESS_KEY_ID;
+  const SECRET_ACESS_KEY_ID = import.meta.env.VITE_APP_SECRET_ACCESS_KEY_ID;
+
+  AWS.config.update({
+    region: REGION,
+    accessKeyId: ACESS_KEY_ID,
+    secretAccessKey: SECRET_ACESS_KEY_ID,
+  });
+
+  console.log(formData, imageFile);
+  const upload = new AWS.S3.ManagedUpload({
+    params: {
+      ACL: 'public-read',
+      Bucket: 'nuwa-image-bucket',
+      Key: `upload/${imageFile.name}`,
+      Body: imageFile,
+    },
+  });
+
+  return upload.promise();
+};
