8aba8f923059307a3fedb0f7e54c5b749cb5ffa8
Jun <yuncheol.kim42@gmail.com>
2024-03-24 13:28:44 +0900

    refactor : 개인 채팅 리팩토링

---
+++ src/pages/directChat/useUpdateDirectChatMessage.js
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+
+const useUpdateDirectChatMessage = ({
+  socketMessageUpdateList,
+  setSocketMessageList,
+  directChatMessageList,
+  setSocketMessageUpdateList,
+}) => {
+  useEffect(() => {
+    if (socketMessageUpdateList.length !== 0) {
+      setSocketMessageList((state) => {
+        const newState = [...state];
+        for (let updateItem of socketMessageUpdateList) {
+          newState.forEach((item) => {
+            if (item.messageId === updateItem.id) {
+              item.messageId = item.messageId + 'update';
+              item.content = updateItem.content;
+            }
+            return item;
+          });
+        }
+        return newState;
+      });
+      if (directChatMessageList.length > 0) {
+        for (let updateItem of socketMessageUpdateList) {
+          directChatMessageList.forEach((item) => {
+            if (item.messageId === updateItem.id) {
+              item.messageId = item.messageId + 'update';
+              item.content = updateItem.content;
+            }
+            return item;
+          });
+        }
+      }
+
+      setSocketMessageUpdateList([]);
+    }
+  }, [socketMessageUpdateList]);
+};
+
+export default useUpdateDirectChatMessage;
