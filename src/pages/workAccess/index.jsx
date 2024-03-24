d67b445b810be01455c01f891e6863278b46451f
hyoungseok <khs43833@gmail.com>
2024-03-25 00:01:37 +0900

    design/ padding 설정

--- src/pages/workAccess/index.jsx
+++ src/pages/workAccess/index.jsx
@@ -85,10 +85,11 @@ const StContainer = styled.div`
 
 const WorkspaceContainer = styled.div`
   width: 100%;
-  height: 800px;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
+  padding: 50px;
 `;
