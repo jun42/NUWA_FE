ed6bed3718050e62fbeacb58e92cc701ec784f6e
hyoungseok <khs43833@gmail.com>
2024-03-25 12:58:22 +0900

    ui 수정

--- src/apis/canvas/petchCanvas.js
+++ src/apis/canvas/petchCanvas.js
@@ -1,7 +1,7 @@
 import { request } from '@apis/axios/axios'; 
 
 export const updateCanvas = async ({ workSpaceId, canvasId, title, content }) => {
-  const response = await request.post(`/canvas/${workSpaceId}?canvasId=${canvasId}`, {
+  const response = await request.patch(`/canvas/${workSpaceId}?canvasId=${canvasId}`, {
     title,
     content,
   });
