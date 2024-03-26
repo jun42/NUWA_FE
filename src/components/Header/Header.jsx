28bd925a66953bd2211a0dfbdab142313ae72ab7
Jun <yuncheol.kim42@gmail.com>
2024-03-26 22:58:01 +0900

    feat: 로그아웃시 토큰제거 주석 해제

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
   };
   const handleLogoutButton = () => {
     logoutRequest();
-    // removeToken();
+    removeToken();
     setIsLoggedIn(false);
     navigate('/login');
   };
